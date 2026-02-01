989efb0170e6568107c1921ec2e959d37174edfa
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-01 21:04:51 +0500

    INFRA: Initialize Build Project

--- Library.Api/Library.Api.Infrastructure.Build/Program.cs
+++ Library.Api/Library.Api.Infrastructure.Build/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var aDotNetClient = new ADotNetClient();
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Library Build Pipeline",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Check out"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setup .Net",
+
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "10.0.102",
+                            IncludePrerelease = true
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restore"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Build"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Test"
+                    }
+                }
+            }
+        }
+    }
+};
+
+string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
