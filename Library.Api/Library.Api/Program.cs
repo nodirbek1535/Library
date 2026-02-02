1397e565b7774eeae06890f7117524eb5da6cb3f
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-02 20:20:46 +0500

    CONFIG: Register Broker

--- Library.Api/Library.Api/Program.cs
+++ Library.Api/Library.Api/Program.cs
@@ -2,9 +2,13 @@
 //@nodirbek1535 library api program (C)
 //===============================================
 
+using Library.Api.Brokers.Storages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StorageBroker>();
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
