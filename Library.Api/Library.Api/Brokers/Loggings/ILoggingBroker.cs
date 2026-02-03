d61a8b62375eb08c167a2a0f0bf37f8bad9efb5b
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-03 19:50:32 +0500

    BROKERS: Implement Logging

---
+++ Library.Api/Library.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+namespace Library.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+
+    }
+}
---
+++ Library.Api/Library.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,20 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+namespace Library.Api.Brokers.Loggings
+{
+    public class LoggingBroker:ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+    }
+}
