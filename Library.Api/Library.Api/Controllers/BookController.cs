a56460a43a73b9aecfe5c0406a1e682b73ae2140
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-05 17:17:52 +0500

    CONTROLLERS: Add PostBookAsync

---
+++ Library.Api/Library.Api/Controllers/BookController.cs
@@ -0,0 +1,50 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using Library.Api.Models.Books;
+using Library.Api.Models.Books.Exceptions;
+using Library.Api.Services.Foundations.Books;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Library.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : RESTFulController
+    {
+        private readonly IBookService bookService;
+
+        public BookController(IBookService bookService) =>
+            this.bookService = bookService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult> PostBookAsync(Book book)
+        {
+            try
+            {
+                Book addedBook = await this.bookService.AddBookAsync(book);
+
+                return Created(addedBook);
+            }
+            catch(BookValidationException bookValidationException)
+            {
+                return BadRequest(bookValidationException.InnerException);
+            }
+            catch (BookDependencyValidationException bookDependencyValidationException)
+                when (bookDependencyValidationException.InnerException is AlreadyExistBookException)
+            {
+                return Conflict(bookDependencyValidationException.InnerException);
+            }
+            catch(BookDependencyException bookDependencyException)
+            {
+                return InternalServerError(bookDependencyException.InnerException);
+            }
+            catch(BookServiceException bookServiceException)
+            {
+                return InternalServerError(bookServiceException.InnerException);
+            }
+        }
+    }
+}
