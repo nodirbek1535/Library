fe7be77a80831ca9c5a6daf94c83a9807c882f5b
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-06 19:04:49 +0500

    ShouldRetrieveBookByIdAsync -> PASS

---
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,47 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Library.Api.Models.Books;
+using Microsoft.AspNetCore.Components;
+using Moq;
+
+namespace Library.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveBookByIdAsync()
+        {
+            //given
+            Guid randomBookId = Guid.NewGuid();
+            Guid inputBookId = randomBookId;
+
+            Book randomBook = CreateRandomBook();
+            Book persistedBook = randomBook;
+            Book expectedBook = persistedBook;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(inputBookId))
+                    .ReturnsAsync(persistedBook);
+
+            //when
+            Book actualBook =
+                await this.bookService.RetrieveBookByIdAsync(inputBookId);
+
+            //then
+            actualBook.Should().BeEquivalentTo(expectedBook);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(inputBookId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();    
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
