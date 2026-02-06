ef065409514fb3efbb0b619c4ca20c1dd6b8adaa
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-02-06 19:08:52 +0500

    ShouldThrowSqlExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,55 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Api.Models.Books;
+using Library.Api.Models.Books.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace Library.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowSqlExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someBookId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedBookStorageException =
+                new FailedBookStorageException(sqlException);
+
+            var expectedBookDependencyException =
+                new BookDependencyException(failedBookStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(someBookId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Book> retrieveBookByIdTask =
+                this.bookService.RetrieveBookByIdAsync(someBookId);
+
+            //then
+            await Assert.ThrowsAsync<BookDependencyException>(() =>
+                retrieveBookByIdTask.AsTask()); 
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(someBookId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedBookDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
