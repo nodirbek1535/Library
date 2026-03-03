d242f1d27b6a9b8db38fb7a74f23a4491a0f2810
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-03 22:30:52 +0500

    ShouldThrowValidationExceptionOnRemoveIfBookIdIsInvalidAndLogItAsync -> PASS

--- Library.Api/Library.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Validations.Remove.cs
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Validations.Remove.cs
@@ -14,6 +14,42 @@ namespace Library.Api.Tests.Unit.Services.Foundations.Books
     public partial class BookServiceTests
     {
         [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfBookIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidBookId = Guid.Empty;
+
+            var invalidBookException =
+                new InvalidBookException();
+
+            invalidBookException.AddData(
+                key: nameof(Book.Id),
+                values: "Id is required");
+
+            var expectedBookValidationException =
+                new BookValidationException(invalidBookException);
+
+            //when
+            ValueTask<Book> removeBookTask =
+                this.bookService.RemoveBookByIdAsync(invalidBookId);
+
+            //then
+            await Assert.ThrowsAsync<BookValidationException>(() =>
+                removeBookTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedBookValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldThrowValidationsExceptionOnRemoveIfBookDoesNotExistAndLogItAsync()
         {
             //given
