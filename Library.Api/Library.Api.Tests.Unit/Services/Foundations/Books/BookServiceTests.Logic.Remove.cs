311663f5148ea8c1e6a2815de7b683f732eaf795
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-05 20:43:11 +0500

    ShouldThrowRemoveBookByIdAsync -> PASS

---
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.Remove.cs
@@ -0,0 +1,53 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Library.Api.Models.Books;
+using Moq;
+
+namespace Library.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowRemoveBookByIdAsync()
+        {
+            //given
+            Book randomBook = CreateRandomBook();
+            Book storedBook = randomBook;
+            Book deletedBook = storedBook;
+            Guid bookId = storedBook.Id;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(bookId))
+                    .ReturnsAsync(storedBook);
+
+             this.storageBrokerMock.Setup(broker =>
+                broker.DeleteBookAsync(deletedBook))
+                    .ReturnsAsync(deletedBook);
+
+            //when
+            Book actualBook =
+                await this.bookService.RemoveBookByIdAsync(bookId);
+
+            //then
+            actualBook.Should().BeEquivalentTo(deletedBook);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(bookId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteBookAsync(deletedBook),
+                    Times.Once);
+
+             this.storageBrokerMock.VerifyNoOtherCalls();
+             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
