69343f18d5f9110ae374422b02ebb78349268be8
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-18 02:24:47 +0500

    SERVICES: Add RetrieveReaderByIdAsync

---
+++ Library.Api/Library.Api/Models/Readers/Exceptions/NotFoundReaderException.cs
@@ -0,0 +1,16 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+
+using Xeptions;
+
+namespace Library.Api.Models.Readers.Exceptions
+{
+    public class NotFoundReaderException : Xeption
+    {
+        public NotFoundReaderException(Guid readerId)
+            : base(message: $"Couldn't find reader with id: {readerId}")
+        { }
+    }
+}
