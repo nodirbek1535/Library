c08e52479e9cd925ae26549a80ec33ec959da232
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-18 14:12:20 +0500

    ShouldThrowRetrieveAllReaders -> PASS

---
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,42 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Library.Api.Models.Readers;
+using Moq;
+
+namespace Library.Api.Tests.Unit.Services.Foundations.Readers
+{
+    public partial class ReaderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowRetrieveAllReaders()
+        {
+            //given
+            IQueryable<Reader> randomReaders = CreateRandomReaders();
+            IQueryable<Reader> persistedReaders = randomReaders;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllReaders())
+                    .Returns(persistedReaders);
+
+            //when
+            IQueryable<Reader> actualReaders =
+                this.readerService.RetrieveAllReaders();
+
+            //them
+            actualReaders.Should().BeEquivalentTo(persistedReaders);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectAllReaders(),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
