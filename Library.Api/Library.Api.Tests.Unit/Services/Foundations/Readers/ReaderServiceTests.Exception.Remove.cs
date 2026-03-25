8908ff99174cf8afbf1327e02c7dbc0b383297c5
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-25 10:21:06 +0500

    ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Library.Api/Library.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exception.Remove.cs
@@ -0,0 +1,55 @@
+﻿//===============================================
+//@nodirbek1535 library api program (C)
+//===============================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Api.Models.Readers;
+using Library.Api.Models.Readers.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace Library.Api.Tests.Unit.Services.Foundations.Readers
+{
+    public partial class ReaderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someReaderId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedReaderStorageException =
+                new FailedReaderStorageException(sqlException);
+
+            var expectedReaderDependencyException =
+                new ReaderDependencyException(failedReaderStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectReaderByIdAsync(someReaderId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Reader> removeReaderTask =
+                this.readerService.RemoveReaderByIdAsync(someReaderId);
+
+            //then
+            await Assert.ThrowsAsync<ReaderDependencyException>(() =>
+                removeReaderTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedReaderDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReaderByIdAsync(someReaderId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
